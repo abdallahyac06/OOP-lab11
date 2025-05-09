4c0f64c2dfd81d5108d66b1977709f9271a2170e
abdallahyac06 <abdallahyac06gpt@gmail.com>
2025-05-09 18:27:15 +0100

    Add driver

--- DoubleSubscriptedArray/main.cpp
+++ DoubleSubscriptedArray/main.cpp
@@ -3,7 +3,23 @@
 
 int main() {
     Table<int> t(3, 2);
-    std::cin >> t;
-    std::cout << t;
+    t(0, 0) = 1;
+    t(0, 1) = 2;
+    t(1, 0) = 3;
+    t(1, 1) = 4;
+    t(2, 0) = 5;
+    t(2, 1) = 6;
+    std::cout << "Table t:\n" << t << std::endl;
+
+    Table<int> t2(2, 2);
+    std::cout << "Table t2:\n" << t2 << std::endl;
+    t2 = t;
+    if (t == t2) {
+        std::cout << "Tables are equal after assignment." << std::endl;
+    } else {
+        std::cout << "Tables are not equal after assignment." << std::endl;
+    }
+    std::cout << "Table t2 after assignment:\n" << t2 << std::endl;
+
     return 0;
 }
